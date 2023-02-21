b25470bfd5daa74f4b8dd8a73d34cd9813ae5da3
Enamerherhe Justice <justiceenams@gmail.com>
2023-02-21 10:10:47 +0100

    time correction

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var date = new Date(2023,3,20,19)
+var date = new Date()
 var time = date.getHours()
 
  var adStyle = document.querySelector("h2")
@@ -9,17 +9,14 @@ if (time < 12) {
     document.querySelector(".greetings").innerHTML = "Good morning" 
     adStyle.classList.add("hello")
     
-}else if (time < 18) {
-    document.querySelector(".greetings").innerHTML = "Good afternoon"
+} else if (time < 16) {
+    document.querySelector(".greetings").innerHTML = "Good afternoon"     
     adStyle.classList.add("hello")
 
-} else if (time < 20) {
-    document.querySelector(".greetings").innerHTML = "Good evening"
+} else {
+    document.querySelector(".greetings").innerHTML = "Good evening" 
     adStyle.classList.add("hello")
 }
-else {
-    document.querySelector(".greetings").innerHTML = "Good night"
-    adStyle.classList.add("hello")
-}
+
 
 
